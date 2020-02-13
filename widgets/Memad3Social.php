1ee4fe8ad87774f7efc70e63bbc7200456534c6f
Meni Nuriel <nurielmeni@gmail.com>
2020-02-14 01:07:01 +0200

    Migration add column identifier to theme

--- widgets/Memad3Social.php
+++ widgets/Memad3Social.php
@@ -14,7 +14,7 @@ class Memad3Social extends \yii\bootstrap\Widget
         $res = '<ul class="social pull-left nav navbar-nav flex">';
         foreach ($this->socials as $name => $url) {
             if ($name === 'ins') {
-                $res .= '<li style="width: 40px; height: 40px; background-image: linear-gradient(to right, #00c0ff 0%, #88ffff 100%); border-radius: 20px; margin: 5px;"><a style="background: url(/images/socialShare/instegram-white.png) no-repeat center center; background-size: contain; margin: 5px;" href="' . $url . '"></a></li>';
+                $res .= '<li style="width: 40px; height: 40px; background-image: linear-gradient(to right, #00c0ff 0%, #88ffff 100%); border-radius: 20px; margin: 5px;"><a style="background: url(\'/images/socialShare/instegram-white.png\') no-repeat center center; background-size: contain; margin: 5px;" href="' . $url . '"></a></li>';
             } else {
                 $res .= '<li style="width: 40px; height: 40px; background-image: linear-gradient(to right, #00c0ff 0%, #88ffff 100%); border-radius: 20px; margin: 5px;"><a style="padding: 10px 0; text-align: center; color: #fff; font-size: 22px; font-weight: bold;" href="' . $url . '">' . $name . '</a></li>';
             }
