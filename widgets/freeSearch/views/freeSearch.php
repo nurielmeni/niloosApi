3b3b3b924958325beab720761d9a3b0695afe885
Meni Nuriel <nurielmeni@gmail.com>
2020-02-17 23:55:33 +0200

    Merge origin/quill into quill
    Conflicts:
    	backend/modules/attributer/assets/js/attributer.js
    	backend/web/images/spinner.gif

--- widgets/freeSearch/views/freeSearch.php
+++ widgets/freeSearch/views/freeSearch.php
@@ -29,7 +29,7 @@ use yii\bootstrap\ActiveForm;
         <?= $form->field($model, 'freetext', ['options' => ['class' =>  $inline ? 'form-group flex' : 'form-group']])->textInput([
             'prompt' => $model->getAttributeLabel('freeText'),
             'class' => 'free-text',
-        ])->label($prompt) ?>
+        ])->label($prompt, ['class' => 'hidden-xs']) ?>
 
         <div class="form-group">
             <?= Html::submitButton($submitLabel, ['class' => 'btn memad3 light-blue fg-white', 'name' => 'search-button']) ?>
