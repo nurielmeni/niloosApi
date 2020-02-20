2049bae6050cc29c5f077b9181baaeafa6377afd
Meni Nuriel <nurielmeni@gmail.com>
2020-02-20 12:10:32 +0200

    Theme init

--- widgets/socialShare/SocialShare.php
+++ widgets/socialShare/SocialShare.php
@@ -17,7 +17,7 @@ class SocialShare extends \yii\bootstrap\Widget
         ['name' => 'Twitter', 'url' => 'https://twitter.com/intent/tweet?url=', 'img' => 'twitter.png', 'mobile' => false],
         ['name' => 'LinkedIn', 'url' => 'https://www.linkedin.com/shareArticle?mini=true&url=', 'img' => 'in.png', 'mobile' => false],
         ['name' => 'Facebook', 'url' => 'https://www.facebook.com/dialog/share?app_id=149701323135240&display=popup&href=', 'img' => 'facebook.png', 'mobile' => false],
-        ['name' => 'Instegram', 'url' => 'https://instagram.com?url=', 'img' => 'instegram.png', 'mobile' => false],
+        //['name' => 'Instegram', 'url' => 'https://instagram.com?url=', 'img' => 'instegram.png', 'mobile' => false],
         ['name' => 'Whatsapp', 'url' => 'https://wa.me/?text=', 'img' => 'whatsapp.png', 'mobile' => false],
     ];
     public $shareUrl = '';
