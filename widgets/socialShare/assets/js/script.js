e24349f06653211ca7ffd293fccead64c08279c2
Meni Nuriel <nurielmeni@gmail.com>
2020-02-20 02:44:36 +0200

    Merge origin/quill into quill
    Conflicts:
    	backend/modules/attributer/assets/js/attributer.js
    	backend/web/images/spinner.gif

--- widgets/socialShare/assets/js/script.js
+++ widgets/socialShare/assets/js/script.js
@@ -6,19 +6,43 @@
 
 
 $(document).ready(function() {
-    $('.social-share .social-toggle').click(function(){
-        var $button = $(this);
-        $(this).css('z-index', -1).siblings('.outer').animate({
-            width: 'toggle'
-        }, {
-            duration: 400,
-            easing: 'linear',
-            complete: function() { $button.css('z-index', 10); }
+    if (navigator.share) {
+        const shareData = {
+            title: 'MEMAD3',
+            text: 'Find you next job!',
+            url: 'https://memad3.hunterhrms.com',
+        }
+        
+        const btn = document.querySelector('.social-share .social-toggle');
+        const resultPara = document.querySelector('.result');
+
+        // Must be triggered some kind of "user activation"
+        btn.addEventListener('click', async () => {
+            try {
+              await navigator.share(shareData)
+            } catch(err) {
+              console.log('Error: ' + e);
+            }
+            console.log('MEMAD3 shared successfully');
         });
-    });
-    
-    $('.social-share .inner img').on('click', function() {
-        var url = $(this).data('url');
-        window.open(url,'_blank');
-    });
+    } else {
+        $('.social-share .social-toggle').click(function(){
+            var $button = $(this);
+            $(this).css('z-index', -1).siblings('.outer').animate({
+                width: 'toggle'
+            }, {
+                duration: 400,
+                easing: 'linear',
+                complete: function() { $button.css('z-index', 10); }
+            });
+        });
+
+
+
+        $('.social-share .inner img').on('click', function() {        
+            var url = $(this).data('url');
+            window.open(url,'_blank');
+        });
+    }
+
 });
