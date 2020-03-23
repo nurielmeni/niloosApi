c90cc8771332d2ce2111b820823b8203f8d0deac
Meni Nuriel <nurielmeni@gmail.com>
2020-03-24 01:15:05 +0200

    Attributer improved image uploader

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=db;dbname=memad',
+    'username' => 'memad',
+    'password' => 'memad',
+    'charset' => 'utf8mb4',
+];
