02db413502c4012f9178d19abc16d3873aad99e4
Meni Nuriel <nurielmeni@gmail.com>
2020-05-21 01:33:00 +0300

    Update Params

--- config/params.php
+++ config/params.php
@@ -1,13 +1,5 @@
 <?php
 
 return [
-    'adminEmail' => 'info@memad3.com',
-    'bccEmail' => 'meni-nuriel@hotmail.com',
-    'senderEmail' => 'memad3@cvwebmail.net',
-    'senderName' => 'Memad-3 Jobs',
-    'cvWebMail' => 'memad3@cvwebmail.net',
-    'memadIn' => 'https://www.linkedin.com/company/memad-3/?viewAsMember=true',
-    'memadFb' => 'https://www.facebook.com/Memad3Jobs/',
-    'memadIns' => 'https://instagram.com/memad_3?igshid=mppbqkpg7apq',
     'flushCache' => false,
 ];
